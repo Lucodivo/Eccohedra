6587dc71aeb136eda84d14775c879280b244ca57
Connor Haskins <ConnorAHaskins@gmail.com>
2022-05-07 21:58:15 -0700

    update to androidx

--- app/src/androidTest/java/com/inasweaterpoorlyknit/learnopengl_androidport/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/inasweaterpoorlyknit/learnopengl_androidport/ExampleInstrumentedTest.kt
@@ -1,22 +1,20 @@
 package com.inasweaterpoorlyknit.learnopengl_androidport
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.junit.runner.RunWith
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
+        val appContext = ApplicationProvider.getApplicationContext<Context>()
         assertEquals("com.inasweaterpoorlyknit.learnopengl_androidport", appContext.packageName)
     }
 }
