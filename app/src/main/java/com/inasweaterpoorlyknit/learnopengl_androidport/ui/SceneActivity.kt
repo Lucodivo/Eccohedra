c3665a83c243727bf9c52f0f634ad31bc44fde4d
Connor Haskins <ConnorAHaskins@gmail.com>
2022-05-12 14:05:41 -0700

    HomeViewModel

--- app/src/main/java/com/inasweaterpoorlyknit/learnopengl_androidport/ui/SceneActivity.kt
+++ app/src/main/java/com/inasweaterpoorlyknit/learnopengl_androidport/ui/SceneActivity.kt
@@ -1,14 +1,13 @@
-package com.inasweaterpoorlyknit.learnopengl_androidport.activities
+package com.inasweaterpoorlyknit.learnopengl_androidport.ui
 
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.inasweaterpoorlyknit.learnopengl_androidport.graphics.scenes.InfiniteCubeScene
-import com.inasweaterpoorlyknit.learnopengl_androidport.graphics.Scene
 import com.inasweaterpoorlyknit.learnopengl_androidport.graphics.SceneSurfaceView
 import com.inasweaterpoorlyknit.learnopengl_androidport.utils.hideSystemUI
 
-class SceneActivity<T: Scene> : AppCompatActivity() {
+class SceneActivity : AppCompatActivity() {
 
   private lateinit var scene: SceneSurfaceView
 
