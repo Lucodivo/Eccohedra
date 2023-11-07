9ad9acd2282033071c5648a802271ab668b443d9
Connor Haskins <ConnorAHaskins@gmail.com>
2023-11-06 16:39:03 -0800

    lock orientation for native activities

--- app/src/main/java/com/inasweaterpoorlyknit/learnopengl_androidport/graphics/native_activities.kt
+++ app/src/main/java/com/inasweaterpoorlyknit/learnopengl_androidport/graphics/native_activities.kt
@@ -1,6 +1,8 @@
 package com.inasweaterpoorlyknit.learnopengl_androidport.graphics
 
 import android.app.NativeActivity
+import android.content.pm.ActivityInfo
+import android.os.Bundle
 
 /*
     NativeActivity is an Activity that is entirely controlled through native code (cpp)
@@ -9,5 +11,24 @@ import android.app.NativeActivity
     This project uses native activities as a way of more easily porting code between Windows and Android
     with the hopes of pulling out a little extra performance along the way.
  */
-class TestNativeActivity: NativeActivity()
-class GateNativeActivity: NativeActivity()
+open class NativeSceneActivity: NativeActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Lock orientation for the duration of the scene
+        requestedOrientation = when(orientation) {
+            Orientation.Portrait -> ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+            Orientation.Landscape -> ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+            Orientation.PortraitReverse -> ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT
+            Orientation.LandscapeReverse -> ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // release orientation to OS
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+    }
+}
+
+class TestNativeActivity: NativeSceneActivity()
+class GateNativeActivity: NativeSceneActivity()
